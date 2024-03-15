ea1fa501842230810c099d7f3bc63f323513d9a5
Rori <rbmasilo@gmail.com>
2024-03-15 11:00:02 +0200

    error debugged

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function validateSyntax() {
   // Validation logic goes here
   let result = " "; // Placeholder for validation result
 
-  let regex = /^pet_\d*[a-zA-Z0-9]+$/;
+  let regex = /^pet_[a-zA-Z0-9]+$/;
 
-    // TODO: Write your validation logic here
+  // TODO: Write your validation logic here
   // Check if input starts with 'pet_' and followed by alphanumeric characters
   if (regex.test(input)) {
-    result = "Valid Syntax <span class='Valid'>U+2705</span>;
+    result = 'Valid Syntax <span class="valid"></span>';
   } else {
-    result = "Invalid Syntax <span class='Invalid'>U+274C</span>;
+    result = 'Valid Syntax <span class="invalid"></span>';
   }
 
   /* let keyboardEvent = new keyboardEvent("keydown", {key: "Enter"});
   document.getElementById("petInput").dispatchEvent(keyboardEvent); */
 
-  document.getElementById("result").innerText = result;
+  document.getElementById("result").innerHTML = result;
 }
