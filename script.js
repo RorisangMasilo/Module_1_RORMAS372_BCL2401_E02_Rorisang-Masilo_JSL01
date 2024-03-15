813ccadf876e29744cf71b899dc395aba5337918
Rori <rbmasilo@gmail.com>
2024-03-15 09:34:36 +0200

    commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function validateSyntax() {
   let input = document.getElementById("petInput").value;
   // Validation logic goes here
-  let result = ""; // Placeholder for validation result
+  let result = " "; // Placeholder for validation result
 
-  // TODO: Write your validation logic here
+  let regex = /^pet_\d*[a-zA-Z0-9]+$/;
 
+    // TODO: Write your validation logic here
   // Check if input starts with 'pet_' and followed by alphanumeric characters
-  if (pet_a - zA - Z0 - 9) {
-    let message = "Valid";
+  if (regex.test(input)) {
+    result = "Valid Syntax <span class='Valid'>U+2705</span>;
   } else {
-    let message = "Invalid";
+    result = "Invalid Syntax <span class='Invalid'>U+274C</span>;
   }
+
+  /* let keyboardEvent = new keyboardEvent("keydown", {key: "Enter"});
+  document.getElementById("petInput").dispatchEvent(keyboardEvent); */
+
   document.getElementById("result").innerText = result;
 }
